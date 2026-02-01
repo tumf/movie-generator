03030dfaa652161d41f14ce40909b86edaeba067
tumf <y.takahara@gmail.com>
2026-02-01 20:04:52 +0900

    chore: bump version

--- src/movie_generator/__init__.py
+++ src/movie_generator/__init__.py
@@ -1,6 +1,6 @@
 """Movie Generator - YouTube slide video generator from blog URLs."""
 
-__version__ = "0.2.19"
+__version__ = "0.2.20"
 
 # Export custom exceptions for convenience
 from .exceptions import (
